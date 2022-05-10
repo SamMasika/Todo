ffaa69a6b5b4d0176eba050cf5bb32d5a07a787a
SamMasika <masikasamwel6@gmail.com>
2022-05-10 09:27:23 +0300

    container

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,19 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Todo List</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 <body class="bg-success">
-    <div class="container w-25 mt-5">
+    <div class="container">
         <div class="card-shadow-sm">
             <div class="card-body">
                 <h3>Todo List</h3>
-                <form method="POST" action="{{route('store')}}" autocomplete="off">
+                <form method="POST" action="{{route('store')}}">
                     @csrf
                     <div class="input-group">
                 <input type="text" name="content" class="form-control" placeholder="Add your new todo">
